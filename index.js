ba79072e9b641e60c298be531aee967a2281bdcd
Ronit001Hub <ronitGithub001@gmail.com>
2024-04-02 11:24:18 +0530

    Upload

---
+++ index.js
@@ -0,0 +1,35 @@
+gsap.to("#page1",{
+  scrollTrigger:{
+    trigger:`#page1`,
+    pin:true,
+    scroller:`#main`,
+    start:`top top`,
+    end:`bottom top`,
+    // markers:true,
+  }
+})
+
+gsap.to("#page2",{
+  scrollTrigger:{
+    trigger:`#page2`,
+    pin:true,
+    scroller:`#main`,
+    start:`top top`,
+    end:`bottom top`,
+    // markers:true,
+    
+  }
+})
+
+gsap.to("#page3",{
+  scrollTrigger:{
+    trigger:`#page3`,
+    pin:true,
+    scroller:`#main`,
+    start:`top top`,
+    end:`bottom top`,
+    // markers:true,
+  }
+})
+
+
